183d292f4722cbe6532b57822c2df821ad88a7b4
Kulsum Shaikh <shaikhkulsum233@gmail.com>
2022-12-15 15:42:51 -0600

    commit tree 12737beb708f71f0888735d028e2cb8db27bef62

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+    validates_presence_of :name, :phone_number
+    validates :name, presence: true, uniqueness: true
+    validates :phone_number, presence: true, length: { is: 10 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+    validates_presence_of :title, :content, :summary, :category
+    validates :title, presence: true, length: { minimum: 4 }, exclusion: { in: ["True Facts"] }
+    validates :content, presence: true, length: { minimum: 250 }
+    validates :summary, presence: true, length: { maximum: 250 }
+    validates :category, presence: true, inclusion: { in: ['Fiction', 'Non-Fiction'] }
 end
